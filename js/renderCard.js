9a37b3eff0084b4bc89d110345facbfa7b6c50fc
va1eon <va1eonmix@gmail.com>
2021-12-09 17:11:53 +0900

    refactoring

--- js/renderCard.js
+++ js/renderCard.js
@@ -1,19 +1,18 @@
+import {createHtmlElement} from './utils.js';
+
 const listCard = document.querySelector('.other-films__list');
 
-const renderCard = (data) => {
+const renderCard = data => {
 
   listCard.textContent = '';
 
   const cards = data.map((item) => {
-    const card = document.createElement('li');
-    card.classList.add('other-films__item');
 
-    const link = document.createElement('a');
-    link.classList.add('other-films__link');
+    const card = createHtmlElement('li', 'other-films__item');
+    const link = createHtmlElement('a', 'other-films__link');
+    const img = createHtmlElement('img', 'other-films__img');
+
     link.dataset.rating = item.vote_average === 0 ? '-' : item.vote_average;
-
-    const img = document.createElement('img');
-    img.classList.add('other-films__img');
     img.alt = `Постер ${item.title || item.name}`;
     img.src = `https://www.themoviedb.org/t/p/w600_and_h900_bestv2${item.poster_path}`;
 
@@ -22,7 +21,6 @@ const renderCard = (data) => {
 
     return card;
   });
-  console.log(cards);
   listCard.append(...cards);
 }
 
